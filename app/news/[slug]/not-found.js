351d7ca3c9329599c0add9ecff77b70f034e55d0
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2024-09-30 12:34:05 +0530

    Updated

---
+++ app/news/[slug]/not-found.js
@@ -0,0 +1,8 @@
+export default function NewsNotFoundPage(){
+    return(
+        <div id="error">
+            <h1>Not Found!</h1>
+           <p>Unfortunately, We could not fine the requested article!</p>
+        </div>
+    );
+}

3feabf735ba37639a1cd3b48fdc0609b63438524
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2024-09-30 12:35:03 +0530

    Updated

--- app/news/page.js
+++ app/news/page.js
@@ -9,7 +9,8 @@ export default function NewsPage(){
     {DUMMY_NEWS.map((newsItem) =>(
         <li key={newsItem.id}>
             <Link href={`/news/${newsItem.slug}`}>
-            <img src={`/images/news/${newsItem.image}`}/>
+            <img src={`/images/news/${newsItem.image}`} alt={newsItem.title}/>
+            <span>{newsItem.title}</span>
             </Link>
         </li>
     ))}

6377a448aab8e8ebc98e56d85291015d291bf517
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2024-10-01 20:18:47 +0530

    Updated

--- app/news/[slug]/page.js
+++ app/news/[slug]/page.js
@@ -13,7 +13,8 @@ export default function NewsDetailPage({params}){
     return (
     <article className="news-article">
     <header>
-    <Link href={'/news/${newsItem.slug}/image'}><img src={`/images/news/${newsItem.image}`} alt={newsItem.title}/></Link>    
+    <Link href={`/news/${newsItem.slug}/image`}>
+    <img src={`/images/news/${newsItem.image}`} alt={newsItem.title}/></Link>    
     <h1>{newsItem.title}</h1>
     <time dateTime={newsItem.date}>{newsItem.date}</time>
     </header>

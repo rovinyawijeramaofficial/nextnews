bb6647e6e5bda098d7de8f1a99dea8654c01d7a8
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2024-10-01 20:18:10 +0530

    Updated

--- components/nav-link.js
+++ components/nav-link.js
@@ -6,8 +6,6 @@ export default function NavLink({href,children}){
     const path = usePathname();
 
     return (
-        <Link href={href}
-                    className={path.startsWith(href) ? 
-                        'active': undefined}>{children}</Link>
+        <Link href={href} className={path.startsWith(href) ? 'active': undefined}>{children}</Link>
     );
-} 
+}

d4202d7c6b825cbbd7cb89b6c0e30027b5d588f4
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2024-10-03 12:21:41 +0530

    Updated

--- app/(content)/news/[slug]/@modal/(.)image/page.js
+++ app/(content)/news/[slug]/@modal/(.)image/page.js
@@ -1,12 +1,12 @@
-'use client';
-import { DUMMY_NEWS } from "@/dummy-news";
-import { notFound, useRouter } from "next/navigation";
+import ModalBackdrop from "@/components/modal-backdrop";
+import { getNewsItem } from "@/lib/news";
 
-export default function InterceptedImagePage({ params }) {
-const router = useRouter();
+import { notFound } from "next/navigation";
+
+export default async function InterceptedImagePage({ params }) {
   
   const newsItemSlug = params.slug;
-  const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsItemSlug);
+  const newsItem = await getNewsItem(newsItemSlug);
 
   if (!newsItem) {
     notFound();  // If news item is not found, this will trigger a 404 response
@@ -14,7 +14,7 @@ const router = useRouter();
 
   return ( 
     <>
-    <div className="modal-backdrop" onClick={router.back}/>
+    <ModalBackdrop/>
     <dialog className="modal" open >
       <div className="fullscreen-image">
         <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
